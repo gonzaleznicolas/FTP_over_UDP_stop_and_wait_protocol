98e50d73ad01a113a055f3e6fbfd356be8edc62a
Nicolas Gonzalez <Nicolas Gonzalez>
2017-02-13 15:41:26 -0700

    the file is turned into an array of arrays of bytes. each array of MAX_PAYLOAD_SIZE (except the last one which may be smaller)

--- FTPClient.java
+++ FTPClient.java
@@ -14,94 +14,101 @@ import java.util.*;
 
 public class FTPClient {
 
-	private String serverName;
-	private int serverPort;
-	private String fileName;
-	private int timeOut;
+    private String serverName;
+    private int serverPort;
+    private String fileName;
+    private int timeOut;
 
-	private Socket socketConnectingToServer;
-	private DataInputStream tcpInputStreamFromServer;
-	private DataOutputStream tcpOutputStreamToServer;
+    private Socket socketConnectingToServer;
+    private DataInputStream tcpInputStreamFromServer;
+    private DataOutputStream tcpOutputStreamToServer;
 
     /**
      * Constructor to initialize the program 
      * 
-     * @param serverName	server name
-     * @param server_port	server port
-     * @param file_name		name of file to transfer
-     * @param timeout		Time out value (in milli-seconds).
+     * @param serverName    server name
+     * @param server_port    server port
+     * @param file_name        name of file to transfer
+     * @param timeout        Time out value (in milli-seconds).
      */
-	public FTPClient(String server_name, int server_port, String file_name, int timeout) {
-	
-	/* Initialize values */
-	serverName = server_name;
-	serverPort = server_port;
-	fileName = file_name;
-	timeOut = timeout;
-	}
-	
+    public FTPClient(String server_name, int server_port, String file_name, int timeout) {
+    
+    /* Initialize values */
+    serverName = server_name;
+    serverPort = server_port;
+    fileName = file_name;
+    timeOut = timeout;
+    }
+    
 
     /**
      * Send file content as Segments
      * 
      */
-	public void send()
-	{
-		try
-		{
-			// INITIALIZE TCP STREAMS
-			socketConnectingToServer = new Socket(serverName, serverPort);
-			tcpInputStreamFromServer = new DataInputStream(socketConnectingToServer.getInputStream());
-			tcpOutputStreamToServer = new DataOutputStream(socketConnectingToServer.getOutputStream());
+    public void send()
+    {
+        try
+        {
+            // INITIALIZE TCP STREAMS
+            socketConnectingToServer = new Socket(serverName, serverPort);
+            tcpInputStreamFromServer = new DataInputStream(socketConnectingToServer.getInputStream());
+            tcpOutputStreamToServer = new DataOutputStream(socketConnectingToServer.getOutputStream());
 
-			// INITIAL HANDSHAKE OVER TCP
-			tcpOutputStreamToServer.writeUTF(fileName);
-			byte response = tcpInputStreamFromServer.readByte();
-			if (response != (byte) 0)
-			{
-				System.out.println("An error occured establishing the TCP connection with the server");
-				System.exit(0);
-			}
+            // INITIAL HANDSHAKE OVER TCP
+            tcpOutputStreamToServer.writeUTF(fileName);
+            byte response = tcpInputStreamFromServer.readByte();
+            if (response != (byte) 0)
+            {
+                System.out.println("An error occured establishing the TCP connection with the server");
+                System.exit(0);
+            }
 
-			// TRANSFER THE FILE OVER UDP
-
-			// make a byte array dataFromFile. it will be of size MAX_PAYLOAD_SIZE unless there are not
-			// enough bytes left to read from the file. in that case, dataFromFile will only be as big as
-			// bytes were left in the file
+            // make an array of arrays of bytes. these arrays will be filled with the bytes from the file.
+            // each array will be of size MAX_PAYLOAD_SIZE, except for the last one which will be smaller.
             FileInputStream fin = null;
+            File f = null;
             boolean bytesLeftToRead = true;
-
+            byte[][] arrayOfChunks = {};
             byte[] dataFromFile = new byte[Segment.MAX_PAYLOAD_SIZE];
-            byte[] temp1;
+            byte[] temp1; // a temporary array
             try
             {
                 fin = new FileInputStream(fileName);
-
+                f = new File(fileName);
+                long fileSize = f.length();
+                int numOfChunks = (int) (fileSize/((int) Segment.MAX_PAYLOAD_SIZE)) + 1;
+                arrayOfChunks = new byte[numOfChunks][];
+                int ithChunk = 0;
                 while(bytesLeftToRead)
                 {
-	                for(int i = 0; i < dataFromFile.length; i++)
-	                {
-	                    int byteReadAsInt = fin.read();
-	                    if (byteReadAsInt == -1)
-	                    {
-	                    	// if we fell in here, the file had fewer than MAX_PAYLOAD_SIZE bytes left to read
-	                    	// so make dataFromFIle byte array just the size needed for the number of bytes we read
-	                    	temp1 = new byte[i];
-	                    	System.arraycopy(dataFromFile, 0, temp1, 0, i);
-	                    	dataFromFile = temp1;
-	                    	bytesLeftToRead = false;	                    	
-	                        break; // break out of for loop
-	                    }
-	                    // if here, we read a byte from the file
-	                    dataFromFile[i] = (byte) byteReadAsInt;
-	                }
-	                System.out.println(dataFromFile.length);
-	                System.out.println(Arrays.toString(dataFromFile));
-	            }
+                    for(int i = 0; i < dataFromFile.length; i++)
+                    {
+                        int byteReadAsInt = fin.read();
+                        if (byteReadAsInt == -1)
+                        {
+                            // if we fell in here, the file had fewer than MAX_PAYLOAD_SIZE bytes left to read
+                            // so make dataFromFIle byte array just the size needed for the number of bytes we read
+                            temp1 = new byte[i];
+                            System.arraycopy(dataFromFile, 0, temp1, 0, i);
+                            dataFromFile = temp1;
+                            bytesLeftToRead = false;                            
+                            break; // break out of for loop
+                        }
+                        // if here, we read a byte from the file
+                        dataFromFile[i] = (byte) byteReadAsInt;
+                    }
+                    //System.out.println(dataFromFile.length);
+                    //System.out.println(Arrays.toString(dataFromFile));
+                    arrayOfChunks[ithChunk] = dataFromFile.clone();
+                    ithChunk++;
+                }
+
+                //System.out.println(Arrays.deepToString(arrayOfChunks));
+
             }
             catch (Exception e)
             {
-                System.out.println("Exception writing to file: " + e.getMessage());
+                System.out.println("Exception reading file: " + e.getMessage());
             }
             finally
             {
@@ -118,54 +125,61 @@ public class FTPClient {
                     System.out.println("error closing file.");
                 }
             }
+            if (arrayOfChunks[arrayOfChunks.length-1].length == 0)
+                arrayOfChunks = Arrays.copyOf(arrayOfChunks, arrayOfChunks.length-1);
+            System.out.println(Arrays.deepToString(arrayOfChunks));
+
+            // at this point, arrayOfChunks is an array of arrays of bytes. all of those bytes together are the file
+            // all of the arrays are MAX_PAYLOAD_SIZE in length, except for the last one which may be smaller.
+
+            // TRANSFER CHUNK BY CHUNK OVER UDP USING RDT 3.0
+            // establish UDP socket and streams
 
 
 
 
 
+        }
+        catch (Exception e)
+        {
+            System.out.println("An exception occured, amigo");
+        }    
 
+    }
 
-		}
-		catch (Exception e)
-		{
-			System.out.println("An exception occured, amigo");
-		}	
-
-	}
-
-	
+    
 
 /**
-     	* A simple test driver
-    	 * 
-     	*/
-	public static void main(String[] args) {
-		
-		String server = "localhost";
-		String file_name = "";
-		int server_port = 8888;
+         * A simple test driver
+         * 
+         */
+    public static void main(String[] args) {
+        
+        String server = "localhost";
+        String file_name = "";
+        int server_port = 8888;
         int timeout = 50; // milli-seconds (this value should not be changed)
 
-		
-		// check for command line arguments
-		if (args.length == 3) {
-			// either provide 3 parameters
-			server = args[0];
-			server_port = Integer.parseInt(args[1]);
-			file_name = args[2];
-		}
-		else {
-			System.out.println("wrong number of arguments, try again.");
-			System.out.println("usage: java FTPClient server port file");
-			System.exit(0);
-		}
+        
+        // check for command line arguments
+        if (args.length == 3) {
+            // either provide 3 parameters
+            server = args[0];
+            server_port = Integer.parseInt(args[1]);
+            file_name = args[2];
+        }
+        else {
+            System.out.println("wrong number of arguments, try again.");
+            System.out.println("usage: java FTPClient server port file");
+            System.exit(0);
+        }
 
-		
-		FTPClient ftp = new FTPClient(server, server_port, file_name, timeout);
-		
-		System.out.printf("sending file \'%s\' to server...\n", file_name);
-		ftp.send();
-		System.out.println("file transfer completed.");
-	}
+        
+        FTPClient ftp = new FTPClient(server, server_port, file_name, timeout);
+        
+        System.out.printf("sending file \'%s\' to server...\n", file_name);
+        ftp.send();
+        System.out.println("file transfer completed.");
+    }
 
 }
